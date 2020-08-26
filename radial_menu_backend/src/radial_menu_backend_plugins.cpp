7725b4055993df4f4e7fd51e41890f1cff4d60d9
Yoshito Okada <okada@rm.is.tohoku.ac.jp>
2020-08-27 06:04:37 +0900

    Trimmed the branch to only the radial_menu_rviz sub directory

--- radial_menu_backend/src/radial_menu_backend_plugins.cpp
+++
@@ -1,5 +0,0 @@
-#include <nodelet/nodelet.h>
-#include <pluginlib/class_list_macros.h>
-#include <radial_menu_backend/backend_nodelet.hpp>
-
-PLUGINLIB_EXPORT_CLASS(radial_menu_backend::BackendNodelet, nodelet::Nodelet);
